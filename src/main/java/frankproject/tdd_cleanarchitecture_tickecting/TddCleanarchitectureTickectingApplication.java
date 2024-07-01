fa1a1c9e5cd10dc2f05ae8e8143abca117afda51
frank <frank220203@gmail.com>
2024-07-02 01:15:14 +0900

    Create Project

---
+++ src/main/java/frankproject/tdd_cleanarchitecture_tickecting/TddCleanarchitectureTickectingApplication.java
@@ -0,0 +1,13 @@
+package frankproject.tdd_cleanarchitecture_tickecting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddCleanarchitectureTickectingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddCleanarchitectureTickectingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/frankproject/tdd_cleanarchitecture_tickecting/TddCleanarchitectureTickectingApplicationTests.java
@@ -0,0 +1,13 @@
+package frankproject.tdd_cleanarchitecture_tickecting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddCleanarchitectureTickectingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
